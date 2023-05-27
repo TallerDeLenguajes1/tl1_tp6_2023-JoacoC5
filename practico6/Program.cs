e7c44a5148d67fe31c99e729bef2065cb9e13679
JoacoC5 <joacocarro05@gmail.com>
2023-05-26 21:09:07 -0300

    Programa invertir numero

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,2 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+int a;
+int b;
+
+a = 10;
+b = a;
+
+Console.WriteLine("valor de a: " + a);
+Console.WriteLine("valor de b: " + b);
+
+int x;
+bool control = int.TryParse(Console.ReadLine(), out x);
+
+int resto = 1, aux = x, i = 0;
+double invertido = 0;
+
+if (control)
+{
+    if (x > 0)
+    {
+        while (aux > 0)
+        {
+            aux = aux / 10;
+            i++;
+        }
+
+        while (x > 0)
+        {
+            resto = x % 10;
+            x = x / 10;
+            invertido = invertido + (resto * (Math.Pow(10, i - 1)));
+            i--;
+        }
+    }
+    Console.WriteLine(invertido);
+}
+else
+{
+    Console.WriteLine("No se ingreso un numero");
+}
